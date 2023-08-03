8fc44e9c1e7ad0f16f80875116b3bbd71b71520c
Pavel Putin <pavelputin2003@yandex.ru>
2023-08-03 15:13:24 +0300

    feat: configure project for work with JPA.
    - Add nessasary beans.
    - Update data sourse properties file.

--- src/main/java/edu/vsu/putin_p_a/config/SpringConfig.java
+++ src/main/java/edu/vsu/putin_p_a/config/SpringConfig.java
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -16,10 +22,12 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("edu.vsu.putin_p_a")
 @PropertySource("classpath:dataSourceConfiguration.properties")
+@EnableJpaRepositories("edu.vsu.putin_p_a.repository")
 @EnableWebMvc
 public class SpringConfig {
     private final ApplicationContext applicationContext;
@@ -64,9 +72,9 @@ public class SpringConfig {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("datasource.driver")));
-        dataSource.setUrl(environment.getProperty("datasource.url"));
-        dataSource.setUsername(environment.getProperty("datasource.name"));
-        dataSource.setPassword(environment.getProperty("datasource.password"));
+        dataSource.setUrl(environment.getRequiredProperty("datasource.url"));
+        dataSource.setUsername(environment.getRequiredProperty("datasource.name"));
+        dataSource.setPassword(environment.getRequiredProperty("datasource.password"));
 
         return dataSource;
     }
@@ -74,5 +82,32 @@ public class SpringConfig {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        final LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("edu.vsu.putin_p_a.models");
+
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendorAdapter);
+        emf.setJpaProperties(hibernateProperties());
+
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+        return transactionManager;
     }
 }
