5fab3dd0849e5b9b9225e0ba7a4aed7eb3df20d4
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-08-05 19:21:06 +0300

    fix: add lazy load on pagination view of book list.

--- src/main/java/edu/vsu/putin_p_a/repository/BooksRepository.java
+++ src/main/java/edu/vsu/putin_p_a/repository/BooksRepository.java
@@ -1,6 +1,8 @@
 package edu.vsu.putin_p_a.repository;
 
 import edu.vsu.putin_p_a.models.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,7 @@ import java.util.List;
 public interface BooksRepository extends JpaRepository<Book, Integer> {
     @EntityGraph(attributePaths = "owner", type = EntityGraph.EntityGraphType.LOAD)
     List<Book> findAll();
+
+    @EntityGraph(attributePaths = "owner", type = EntityGraph.EntityGraphType.LOAD)
+    Page<Book> findAll(Pageable pageable);
 }
